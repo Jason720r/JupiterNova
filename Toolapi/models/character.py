8b9622548dd90d1985dc3cc540143735bd2a4317
Jason Li <jasonli99193@gmail.com>
2024-12-27 17:51:54 -0600

    more models..

--- Toolapi/models/character.py
+++ Toolapi/models/character.py
@@ -1,1 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Character(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=100)
+    alignment = models.ForeignKey("Alignment", on_delete=models.CASCADE, related_title="alignment")
