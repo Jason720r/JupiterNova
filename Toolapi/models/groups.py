6731f05596c1a9561c56ea40553ee1b824a0d764
Jason Li <jasonli99193@gmail.com>
2024-12-27 18:47:18 -0600

    new group model

--- Toolapi/models/groups.py
+++ Toolapi/models/groups.py
@@ -1,1 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Group(models.Model):
+    Name = models.Charfield(max_length=100)
+    aalignment = models.ForeignKey("Alignment", on_delete=models.CASCADE, related_title="alignment")
