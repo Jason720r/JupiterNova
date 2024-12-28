6427d5e43facc13b0ef440c99cf4f2d234ac7f17
Jason Li <jasonli99193@gmail.com>
2024-12-27 21:54:17 -0600

    weapon model

---
+++ Toolapi/models/weapon.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Weapons(models.Model):
+    title = models.CharField(max_length=20)
+    damage = models.CharField(max_length=40)
+    rarity = models.ForeignKey("Rarity", on_delete=models.CASCADE, related_title="rarity")
