c60fe41700c557fee4c1a6444db5a48bf0d85745
Jason Li <jasonli99193@gmail.com>
2025-01-10 12:24:53 -0600

    weapon view

---
+++ Toolapi/views/weaponview.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from Toolapi.models import Weapons
+
+class WeaponView(ViewSet):
+
+    def retrieve(self, request, pk):
+        weapon = Weapons.objects.get(pk=pk)
+        serializer = WeaponSerializer(weapon)
+        return Response(serializer.data)
+    
+    def list(self, request):
+        weapon = Weapon.objects.get(user=request.user)
+        serializer = WeaponSerializer(weapon)
+        return Response(serializer.data)
+
+class WeaponSerializer:
+    
+    class Meta:
+        model = Weapons
+        fields = ('id', 'title', 'damage', 'rarity')
