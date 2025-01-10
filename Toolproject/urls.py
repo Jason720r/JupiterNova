a25d0c787e5e45ef284a6ab5025eeb0d95e2bac4
Jason Li <jasonli99193@gmail.com>
2025-01-10 15:06:08 -0600

    added urls.{

--- Toolproject/urls.py
+++ Toolproject/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
+from Toolapi import views
+from Toolapi.views import CharacterView, weaponview, rarityview, groupview, alignmentview
 from rest_framework import routers
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'characters', CharacterView, 'character')
+router.register(r'weapons', weaponview, 'weapon')
+router.register(r'rarities', rarityview, 'rarity')
+router.register(r'groups', groupview, 'group')
+router.register(r'alignments', alignmentview, 'alignment')
+
 
 urlpatterns = [
     path('', include(router.urls)),
